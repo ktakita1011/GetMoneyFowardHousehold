944c076e82713b3bdc666d20c39aec4017a54d3e
ktakita <ktakita1011@gmail.com>
2021-11-09 00:37:50 +0900

    画面推移までできた

--- get_MoneyFowardHousehold.py
+++ get_MoneyFowardHousehold.py
@@ -1,17 +1,17 @@
+import os
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import time
-import json
 from selenium.webdriver.common.action_chains import ActionChains
-from time import sleep
+from bs4 import BeautifulSoup
 
-browser = webdriver.Chrome('C:\\selenium\\chromedriver.exe')
+browser = webdriver.Chrome(r'C:\slenium\chromedriver.exe')
 
 url = 'https://id.moneyforward.com/sign_in/email'
 browser.get(url)
 sleep(3)
-EMAIL = 'email'
-PASS = 'pass'
+EMAIL = os.environ['MONEYFOWARD']
+PASS = os.environ['MONEYFOWARD_PASS']
 # Enter email
 elem_loginMethod = browser.find_element_by_xpath(
     '/html/body/main/div/div/div/div/div[1]/section/form/div[2]/div/input')
@@ -43,10 +43,21 @@ goto_ME = browser.find_element_by_xpath(
         '/html/body/main/div/div/div/div/div[1]/section/form/div[2]/div/div[2]/input')
 goto_ME.click()
 sleep(3)
-goto_yosan = browser.find_element_by_xpath(
-    '//*[@id="header-container"]/header/div[2]/ul/li[3]/a'
+
+#予算ページに動く
+goto_kakei = browser.find_element_by_xpath(
+    '//*[@id="header-container"]/header/div[2]/ul/li[2]/a'
 )
-goto_yosan.click()
+goto_kakei.click()
+sleep(3)
+
+goto_shushi = browser.find_element_by_xpath(
+    '//*[@id="functions-menu-container"]/ul/li[2]/a'
+)
+goto_shushi.click()
 sleep(3)
 
 
+html = browser.page_source.encode('utf-8')
+soup = BeautifulSoup(html, 'lxml')
+#soup.find_all({'div': 'container-large with-ad'})
